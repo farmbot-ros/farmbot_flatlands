faee9d69fa1acf7fd6036dc952c3f898d9c00344
Trim Bresilla <trim.bresilla@gmail.com>
2025-01-03 08:53:38 +0100

    feat: enhance multi-robot launch configuration management
    - Change the include directive from a header file with `.h` extension to one with `.hpp`
    - Add a new launch file to manage the rerun of multiple robots
    - Implement functionality to load a configuration file for the number of robots
    - Use an opaque function to set up launch actions for each robot in the system
    
    Signed-off-by: Trim Bresilla <trim.bresilla@gmail.com>

--- include/farmbot_flatlands/plugins/imu.hpp
+++ include/farmbot_flatlands/plugins/imu.hpp
@@ -10,7 +10,7 @@
 #include <memory>
 #include <cmath>
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include "farmbot_flatlands/plugins/plugin.hpp"
 
